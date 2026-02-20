14907ae73008be8452470b25fc90245daa5c74e3
aswin-git-dev <tceaswin@gmail.com>
2026-02-20 09:57:37 +0530

    all changes

--- app/src/main/java/com/example/mind_detox/MainActivity.kt
+++ app/src/main/java/com/example/mind_detox/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mind_detox
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mind_detox.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +38,44 @@ class MainActivity : AppCompatActivity() {
             setupActionBarWithNavController(navController, appBarConfiguration)
             bottomNav.setupWithNavController(navController)
         }
+
+        // Show Quote of the Day popup
+        showQuotePopup()
+    }
+
+    private fun showQuotePopup() {
+        val quotes = listOf(
+            "The secret of getting ahead is getting started.",
+            "It always seems impossible until it's done.",
+            "Quality is not an act, it is a habit.",
+            "Believe you can and you're halfway there.",
+            "Your time is limited, so don't waste it living someone else's life."
+        )
+        val randomQuote = quotes.random()
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Quote to Start Your Day")
+            .setMessage(randomQuote)
+            .setPositiveButton("Let's Go!") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        
+        // Handle navigation when an item is selected
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
